2a4e2887ddbb34e2e56ee30358c88ff7697d94ef
David Ko <david.ko@velvetreactor.com>
2021-12-31 13:01:03 -0500

    generate method

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -1,2 +1,24 @@
 class DataPoint < ApplicationRecord
+  UNIT_MAPPING = {
+    weight: "lbs",
+    music: "minutes",
+    jumprope: "jumps",
+    leetcode: "leetcode_id",
+  }.with_indifferent_access
+
+  def self.generate(event_type, value, notes)
+    timestamp = Time.now.to_i
+
+    {
+      event_type: event_type,
+      data: {
+        timestamp: timestamp,
+        timestamp_unit: "seconds",
+        value: value,
+        value_unit: UNIT_MAPPING[event_type],
+      },
+      created_at: Time.at(timestamp),
+      updated_at: Time.at(timestamp),
+    }
+  end
 end
