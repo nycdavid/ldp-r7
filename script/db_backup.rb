75e97b909de00f0213aa472e41afc876f4b3d90c
David Ko <david.ko@velvetreactor.com>
2022-01-03 08:33:40 -0500

    Fix up db backup script

--- script/db_backup.rb
+++ script/db_backup.rb
@@ -1,3 +1,9 @@
-output_file = Time.now.strftime("%Y%m%d-ldp-db")
+require "fileutils"
+
+output_file = Time.now.strftime("%Y%m%d-%H%M")
+source = "./#{output_file}"
+destination = "/Volumes/Storage/ldp-db-backups/#{output_file}"
 
 `PGDATA=~/pgdata pg_dump ldp_development -U postgres -f #{output_file}`
+
+FileUtils.mv(source, destination)
