edb45fd0544de6e91f8c6a0a9d3f1d8e241932aa
David Ko <david.ko@velvetreactor.com>
2022-01-04 07:08:19 -0500

    Rails serve static assets

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,7 @@
 
 export RAILS_ENV=production
 export SECRET_KEY_BASE=foobarbaz
+export RAILS_SERVE_STATIC_FILES=true
 
 git pull origin main
 
--- script/start.sh
+++ script/start.sh
@@ -2,5 +2,6 @@
 
 export RAILS_ENV=production
 export SECRET_KEY_BASE=foobarbaz
+export RAILS_SERVE_STATIC_FILES=true
 
 bundle exec rails s -b 0.0.0.0 --daemon
