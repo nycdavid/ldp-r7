664cf715d36b8a1321987d1b2d7595fde3dbc093
David Ko <david.ko@velvetreactor.com>
2022-01-04 07:03:51 -0500

    script updates

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+export RAILS_ENV=production
+export SECRET_KEY_BASE=foobarbaz
+
 git pull origin main
 
 bundle exec rake assets:precompile
--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-RAILS_ENV=production SECRET_KEY_BASE=foobarbaz bundle exec rails s -b 0.0.0.0 --daemon
+export RAILS_ENV=production
+export SECRET_KEY_BASE=foobarbaz
+
+bundle exec rails s -b 0.0.0.0 --daemon
