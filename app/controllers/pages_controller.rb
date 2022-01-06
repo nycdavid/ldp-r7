1e15652b476703d9a6be2c17c49467f58be4d2dd
David Ko <david.ko@velvetreactor.com>
2022-01-06 07:42:48 -0500

    Adds endpoints, html and JS for cards

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def about_us; end
+
+  def retirement_calculator; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root "data_points#index"
 
   resources :data_points
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get :about_us, to: "pages#about_us"
+  get :retirement_calculator, to: "pages#retirement_calculator"
 end
