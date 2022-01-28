f8bc0296f7e4f6817758e63e85a86a2d9be4fa0e
Hyunjoo Jun <hjj102088@gmail.com>
2022-01-27 21:14:39 -0500

    Added calculation

--- app/javascript/controllers/retirement_calculator_controller.js
+++ app/javascript/controllers/retirement_calculator_controller.js
@@ -1,10 +1,17 @@
 let calculateBtn = document.querySelector(".calculate")
 
-// calculateBtn.addEventListener("click", () => {
-//     alert("You just clicked calculate!");
-// });
+calculateBtn.addEventListener("click", (event) => {
+    let netWorth = document.querySelector("#networth").value
+    let goal = document.querySelector("#goal").value
+    let count = 0;
+    
+    do {
+        count = count + 1
+        netWorth = netWorth * 1.05
+    } while (netWorth <= goal);
+    console.log(count);
+    console.log(netWorth);
 
-calculateBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    calculateBtn.insertAdjacentHTML("afterend", "<h1>Hello!</h1>")
-})
+    let line = document.querySelector(".tagline")
+    line.innerText = `It will take ${count} years to retire.`;
+});
