0405531b4f8746f5b23f3cabf3c9a6271e82a3f3
Hyunjoo Jun <hjj102088@gmail.com>
2022-01-28 21:01:43 -0500

    Added style to retirement calc

--- app/javascript/controllers/retirement_calculator_controller.js
+++ app/javascript/controllers/retirement_calculator_controller.js
@@ -1,17 +1,16 @@
-let calculateBtn = document.querySelector(".calculate")
+let calculateBtn = document.querySelector('.calculate');
 
-calculateBtn.addEventListener("click", (event) => {
-    let netWorth = document.querySelector("#networth").value
-    let goal = document.querySelector("#goal").value
+calculateBtn.addEventListener('click', (event) => {
+    let netWorth = document.querySelector('#networth').value;
+    let goal = document.querySelector('#goal').value;
     let count = 0;
-    
     do {
-        count = count + 1
+        count = count + 1;
         netWorth = netWorth * 1.05
     } while (netWorth <= goal);
     console.log(count);
     console.log(netWorth);
 
-    let line = document.querySelector(".tagline")
-    line.innerText = `It will take ${count} years to retire.`;
-});
+    let message = document.querySelector('.tagline');
+    message.innerText = `It will take you ${count} years to retire.`
+})
