5f1dabffb8f7dd975049e9706ab1cc7aa3b426b7
Hyunjoo Jun <hjj102088@gmail.com>
2022-02-01 20:33:04 -0500

    Generate user scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_124753) do
+ActiveRecord::Schema.define(version: 2022_02_02_012201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2022_01_04_124753) do
   create_table "data_points", force: :cascade do |t|
     t.string "event_type"
     t.json "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
