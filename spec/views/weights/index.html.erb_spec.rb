a2ad819dc1382d127a6cc2ee1e194d2ce3b6c8e4
David Ko <davidsj.ko@gmail.com>
2022-02-16 07:10:30 -0500

    Failing test for net change

--- spec/views/weights/index.html.erb_spec.rb
+++ spec/views/weights/index.html.erb_spec.rb
@@ -5,11 +5,8 @@ RSpec.describe "weights/index", type: :view do
   let_it_be(:weight1) { FactoryBot.create(:weight, user: user, created_at: 10.days.ago) }
   let_it_be(:weight2) { FactoryBot.create(:weight, user: user) }
 
-  before(:each) do
-    assign(:weights, [
-      weight1,
-      weight2,
-    ])
+  before do
+    assign(:weights, Weight.where(id: [weight1.id, weight2.id]))
   end
 
   it "renders a list of weights" do
@@ -31,11 +28,27 @@ RSpec.describe "weights/index", type: :view do
     )
     expect(rendered).to have_selector(
       "table.table tbody > tr:nth-child(1) > td:nth-child(2)",
-      text: weight1.measurement.truncate(2),
+      text: weight1.measurement.truncate(1),
     )
     expect(rendered).to have_selector(
       "table.table tbody > tr:nth-child(1) > td:nth-child(3)",
       text: weight1.user.name,
+    )
+  end
+
+  it "shows the net change between 2 adjacent measurements" do
+    render
+
+    net_change = (weight1.measurement - weight2.measurement).truncate(1)
+    class_name = net_change > 0 ? "gain" : "loss"
+    prefix = class_name == "gain" ? "+" : "-"
+
+    expect(rendered).not_to have_selector(
+      "table.table tbody > tr:nth-child(1) > td span.#{class_name}"
+    )
+    expect(rendered).to have_selector(
+      "table.table tbody > tr:nth-child(2) > td span.#{class_name}",
+      text: "(#{prefix}#{net_change.abs})",
     )
   end
 end
