c9a7691ad9d7e53b5445b06ded91e9615d009157
David Ko <davidsj.ko@gmail.com>
2022-02-16 06:15:00 -0500

    kill and restart process

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,4 +10,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake db:migrate
 
-touch tmp/restart.txt
+kill -9 $(ps aux | grep [p]uma | awk '{print $2;}')
+bundle exec rails s -b 0.0.0.0 --daemon
