7201c6944c97bb457edff89b12c3bd8fee2ff57f
Hyunjoo Jun <hjj102088@gmail.com>
2022-02-16 21:15:33 -0500

    Changed weight display

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -3,7 +3,8 @@ class WeightsController < ApplicationController
 
   # GET /weights or /weights.json
   def index
-    @weights = Weight.all
+    user_id = User.find_by(name: params[:user_name].capitalize).id
+    @weights = Weight.where(user_id: user_id)
   end
 
   # GET /weights/1 or /weights/1.json
