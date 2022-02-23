d1aea469279a18fbbd4327efc4ebb8d4e6b74203
David Ko <davidsj.ko@gmail.com>
2022-02-23 05:24:14 -0500

    Fix constant warning

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-NAMES = %w[David Jane Hajin]
-
 FactoryBot.define do
   factory :user do
-    name { NAMES.sample }
+    name { %w[David Jane Hajin].sample }
   end
 end
