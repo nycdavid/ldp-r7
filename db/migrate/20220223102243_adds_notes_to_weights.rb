5481b64d4631d77fa0dc12e44b0e74d982818746
David Ko <davidsj.ko@gmail.com>
2022-02-23 06:08:39 -0500

    Adds notes field to weights

---
+++ db/migrate/20220223102243_adds_notes_to_weights.rb
@@ -0,0 +1,5 @@
+class AddsNotesToWeights < ActiveRecord::Migration[7.0]
+  def change
+    add_column :weights, :notes, :text
+  end
+end
--- spec/factories/weights.rb
+++ spec/factories/weights.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :weight do
     measurement { rand(0.0..200.0) }
+    notes { "Feelings go here" }
   end
 end
