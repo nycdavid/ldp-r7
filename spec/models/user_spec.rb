dfb7b9aed9346ed69e97e6d8195e43074b50ea54
David Ko <davidsj.ko@gmail.com>
2022-03-07 06:40:44 -0500

    Deletes unused tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,16 +14,4 @@ RSpec.describe User, type: :model do
 
     user.weights
   end
-
-  describe "#weights_in_order" do
-    it "returns the weights of a user in the order that they were created" do
-      ordered_weights = user.weights_in_order
-
-      ordered_created_ats = ordered_weights.pluck(:created_at)
-
-      expect(ordered_weights.count).to eq(3)
-      expect(ordered_created_ats[0]).to be < ordered_created_ats[1]
-      expect(ordered_created_ats[1]).to be < ordered_created_ats[2]
-    end
-  end
 end
