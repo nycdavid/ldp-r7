e1363a91da2b661b357beacaa22e4cfe51e039cd
Hyunjoo Jun <hjj102088@gmail.com>
2022-03-11 13:54:31 -0500

    created test for creating new tasks

--- spec/system/tasks_system_spec.rb
+++ spec/system/tasks_system_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe "Tasks", type: :system do
       expect(task.reload.completed_at).not_to eq(nil)
     end
   end
+
+  describe "creating tasks" do
+    it "creates tasks" do
+      FactoryBot.create(:user)
+      visit new_task_path
+      fill_in "Name", with: "Write an Example"
+      fill_in "Description", with: "Figure out how to write an example"
+      fill_in "Start time", with: 0.hours.from_now
+      fill_in "End time", with: 1.hour.from_now
+      click_button "Create Task"
+
+      expect(page).to have_text("Task was successfully created")
+    end
+  end
 end
