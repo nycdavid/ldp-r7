11ef5dec949e07991786db29b9cb270546528110
David Ko <davidsj.ko@gmail.com>
2022-03-11 11:57:14 -0500

    Use FactoryBot for Nodejs sample data

--- spec/__tests__/factories.ts
+++ spec/__tests__/factories.ts
@@ -6,64 +6,56 @@ import Task from "../../app/javascript/resources/task";
 import { Weight as _Weight } from "../../app/javascript/resource_types";
 import { Task as _Task } from "../../app/javascript/resource_types";
 
+import Client from "ioredis";
+import { randomBytes } from "crypto";
+
+const railsChannel = "test_factories_rails";
+
 class Factory {
-  static create(resource: string, attrs = {}) {
-    const { klass, generator } = resourceMap[resource];
+  static async create(resource: string, attrs = {}) {
+    const klass = resourceMap[resource];
 
     const data = {
-      ...generator(),
+      ...(await generator(resource)),
       ...attrs,
     }
 
     return new klass(data);
   }
 
-  static attributesFor(resource: string, attrs = {}) {
-    const { generator } = resourceMap[resource]
-
+  static async attributesFor(resource: string, attrs = {}) {
     return {
-      ...generator(),
+      ...(await generator(resource)),
       ...attrs,
     }
   }
 }
 
-const resourceMap = {
-  weight: {
-    klass: Weight,
-    generator: (): _Weight => {
-      return {
-        date: "2022/01/01",
-        measurement: Math.random() * 200,
-        user: {
-          id: Math.floor(Math.random() * 1000),
-          name: "David",
-        },
-      }
-    },
-  },
-  task: {
-    klass: Task,
-    generator: (): _Task => {
-      const id = Math.floor(Math.random() * 1000);
-      const currentTime = DateTime.now().setZone("UTC");
+const wait = (timeToDelay) => {
+  return new Promise((resolve) => (setTimeout(resolve, timeToDelay)));
+}
 
-      return {
-        id: id,
-        name: `Task #${Math.floor(Math.random() * 1000)}`,
-        description: "Lorem ipsum dolor sit amet",
-        start_time: currentTime.toISO(),
-        end_time: currentTime.plus({ hours: 3 }).toISO(),
-        completed_at: null,
-        routes: {
-          show: `/tasks/${id}`,
-          update: `/tasks/${id}`,
-          delete: `/tasks/${id}`,
-          edit: `/tasks/${id}/edit`,
-        },
-      }
-    },
-  },
+const generator = async (resource) => {
+  const redisA = new Client({ host: "localhost" });
+  const channelId = randomBytes(5).toString("hex");
+  await redisA.publish(railsChannel, `${channelId}:||:${resource}`);
+
+  let data = null;
+  while (data === null) {
+    data = await redisA.get(channelId);
+    await wait(10);
+  }
+  redisA.quit();
+
+  return {
+    ...JSON.parse(data),
+    id: Math.floor(Math.random() * 1000),
+  }
+}
+
+const resourceMap = {
+  weight: Weight,
+  task: Task,
 }
 
 export default Factory;
