a226cf22063ef2af3d536eddce8dfde71a52ee69
David Ko <davidsj.ko@gmail.com>
2022-03-18 06:56:27 -0400

    Adds user association

--- spec/factories/weights.rb
+++ spec/factories/weights.rb
@@ -1,16 +1,19 @@
-FactoryBot.define do
-  LINES = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ",
-    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ",
-    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ",
-    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ",
-    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ",
-    "occaecat cupidatat non proident, sunt in culpa qui officia deserunt ",
-    "mollit anim id est laborum.",
-  ]
+# frozen_string_literal: true
 
+LINES = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ",
+  "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ",
+  "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ",
+  "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ",
+  "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ",
+  "occaecat cupidatat non proident, sunt in culpa qui officia deserunt ",
+  "mollit anim id est laborum.",
+].freeze
+
+FactoryBot.define do
   factory :weight do
     measurement { rand(0.0..200.0) }
     notes { LINES.sample }
+    user
   end
 end
